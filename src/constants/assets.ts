3632063d438a1a527c8eb4c535b6b8dc0630ec96
Antenaina Rarihaja-Razanadrakoto <rarihaja.antenaina@gmail.com>
2024-08-06 01:10:25 +0300

    add sidebar icons

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,5 +1,14 @@
 export const ASSETS = {
     icons: {
+        clock: '/icons/clock.svg',
+        drawer: '/icons/drawer.svg',
+        folders: '/icons/folders.png',
+        lock: '/icons/lock.png',
         logo: '/icons/logo.svg',
+        miniAdd: '/icons/mini-add.svg',
+        miniAddBlue: '/icons/mini-add-blue.svg',
+        notes: '/icons/notes.svg',
+        search: '/icons/search.svg',
+        star: '/icons/star.png',
     },
 }
