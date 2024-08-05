009164f2b08242d598a40834873f49006e9bd0d4
Antenaina Rarihaja-Razanadrakoto <rarihaja.antenaina@gmail.com>
2024-08-05 14:49:28 +0300

    dev experience config

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import { configs as tsConfigs } from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+    js.configs.recommended,
+    tsConfigs.recommended,
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parser: "@typescript-eslint/parser",
+            sourceType: "module",
+            ecmaVersion: 2020,
+        },
+        plugins: {
+            prettier
+        },
+        extends: [
+            "plugin:prettier/recommended",
+            "next/core-web-vitals"
+        ],
+        rules: {
+            ...tsConfigs.recommended.rules,
+            ...js.configs.recommended.rules,
+            "prettier/prettier": "error"
+        },
+        settings: {
+            "import/resolver": {
+                typescript: {}
+            }
+        }
+    }
+];
