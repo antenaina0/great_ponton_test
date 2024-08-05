3009f679d9805c6e24dbb3bd51de04c8829334c4
Antenaina Rarihaja-Razanadrakoto <rarihaja.antenaina@gmail.com>
2024-08-05 17:12:05 +0300

    fonts

---
+++ src/lib/fonts.tsx
@@ -0,0 +1,31 @@
+import { css } from 'styled-components'
+
+export const fonts = css`
+    @font-face {
+        font-family: Gilroy;
+        src: url('/fonts/gilroy/Gilroy-Regular.ttf') format('truetype');
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: Gilroy;
+        src: url('/fonts/gilroy/Gilroy-Medium.ttf') format('truetype');
+        font-weight: 500;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: Gilroy;
+        src: url('/fonts/gilroy/Gilroy-SemiBold.ttf') format('truetype');
+        font-weight: 600;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: Gilroy;
+        src: url('/fonts/gilroy/Gilroy-Bold.ttf') format('truetype');
+        font-weight: 700;
+        font-style: normal;
+    }
+`
