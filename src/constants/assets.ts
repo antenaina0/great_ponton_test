832cbd2b34c2c8a81d7bc04f2702a13a713909ac
Antenaina Rarihaja-Razanadrakoto <rarihaja.antenaina@gmail.com>
2024-08-06 16:21:12 +0300

    gear icon

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -3,6 +3,7 @@ export const ASSETS = {
         clock: '/icons/clock.svg',
         drawer: '/icons/drawer.svg',
         folders: '/icons/folders.png',
+        gear: '/icons/gear.svg',
         lock: '/icons/lock.png',
         logo: '/icons/logo.svg',
         miniAdd: '/icons/mini-add.svg',
