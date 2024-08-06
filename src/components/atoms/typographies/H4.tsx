068cc2b778d77a3de4b1dfc1dbc92149d27fd220
Antenaina Rarihaja-Razanadrakoto <rarihaja.antenaina@gmail.com>
2024-08-07 02:56:31 +0300

    fix H4

--- src/components/atoms/typographies/H4.tsx
+++ src/components/atoms/typographies/H4.tsx
@@ -2,11 +2,17 @@ import { Text, TextProps } from './Text'
 
 interface H4Props extends TextProps {
     children?: React.ReactNode
+    className?: string
 }
 
 export const H4: React.FC<H4Props> = (props) => {
     return (
-        <Text $color="white" $typography="h4" $weight="regular">
+        <Text
+            $color="text"
+            $typography="h4"
+            $weight="semi"
+            className={props.className}
+        >
             {props.children}
         </Text>
     )
