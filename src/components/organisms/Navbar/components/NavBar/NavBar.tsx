4bb7fd98f81bfa29b134860e0d140659005f5653
Antenaina Rarihaja-Razanadrakoto <rarihaja.antenaina@gmail.com>
2024-08-06 16:06:59 +0300

    remove mocks from NavBar component

--- src/components/organisms/Navbar/components/NavBar/NavBar.tsx
+++ src/components/organisms/Navbar/components/NavBar/NavBar.tsx
@@ -1,30 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-import { NavBarSubList, PickItem } from '../../interfaces/nav-bar.interfaces'
+import { NavBarSubList } from '../../interfaces/nav-bar.interfaces'
 import { SubList } from '../SubList/SubList'
 
 export interface SidebarProps {
     subLists?: NavBarSubList[]
-}
-
-const BOARD_HEADER: PickItem = {
-    title: 'My boards',
-    type: 'drawer',
-    header: true,
-    id: 'board1',
-}
-
-const SEARCH_HEADER: PickItem = {
-    title: 'Search',
-    type: 'search',
-    header: true,
-    id: 'search1',
-}
-const TEMPLATE_HEADER: PickItem = {
-    title: 'My templates',
-    type: 'template',
-    header: true,
-    id: 'template1',
 }
 
 export const NavBar: React.FC<SidebarProps> = (props) => {
